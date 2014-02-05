941a0d45d2f1438a649be01a901deac26f7000f9
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2014-02-05 14:30:41 +0000

    Heroku assumes you are uploading RAILS PROJECTS and the database.yml file is an erb file in disguise...so we need to run it through erb

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,6 +3,6 @@ require 'protected_attributes'
 require 'yaml'
 
 class Videatra < Sinatra::Base
-  db = YAML.load_file("#{settings.root}/config/database.yml")
+  db = YAML.load(ERB.new(File.read(File.join("#{settings.root}", "/config/database.yml"))).result)
   ActiveRecord::Base.establish_connection(db[settings.environment.to_s])
 end
